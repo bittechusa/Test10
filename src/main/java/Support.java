2deeb9636849df679fda12ea92a5b8e8428b1435
asadShalok <asadfrombronx@yahoo.com>
2014-04-26 20:32:52 -0400

    add project

---
+++ src/main/java/Support.java
@@ -0,0 +1,20 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.bit.ui.automation.BaseTest;
+
+
+public class Support extends BaseTest
+{
+
+	
+	@Test
+	public void menLink()
+	{
+		
+	}
+	
+
+}
---
+++ src/test/java/com/bit/ui/automation/SmokeTest.java
@@ -0,0 +1,14 @@
+package com.bit.ui.automation;
+
+import org.junit.Test;
+
+public class SmokeTest extends BaseTest 
+{
+	@Test
+	public void menLink()
+	{
+		
+	}
+	
+
+}
